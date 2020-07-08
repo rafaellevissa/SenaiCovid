5683a763d8d44316f356886ef6db71fac44f2050
rafaellevissa <rafaellevissa@gmail.com>
2020-07-08 13:20:30 -0300

    :construction: states location

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
-import 'intl';
-import 'intl/locale-data/jsonp/pt-BR';
 import React from 'react';
 import Routes from './src/routes';
-import 'react-native-gesture-handler';
 
 export default function App() {
   return <Routes />
